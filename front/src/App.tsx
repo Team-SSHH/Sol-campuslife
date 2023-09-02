5d8ead14a3d990dcf8f7b5d9b329a4cf563d8813
윤자현 <bajh323@naver.com>
2023-09-02 20:01:05 +0900

    feat: Create StudentPage, MainPage

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import AppRouter from "./AppRouter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <AppRouter />
     </div>
   );
 }
