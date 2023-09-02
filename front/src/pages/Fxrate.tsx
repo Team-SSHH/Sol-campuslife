1fc4fa61347ac17e96957e00b8978f8f689d64b4
최상익 <qjxltm@naver.com>
2023-09-02 22:25:57 +0900

    feat: 우대율 조회 api 구현

--- front/src/pages/Fxrate.tsx
+++ front/src/pages/Fxrate.tsx
@@ -4,12 +4,19 @@ import axios from "axios";
 interface RateData {
   통화코드: string;
 }
+interface DiscountData {
+  통화: string;
+  우대율: string;
+}
 
 const Fxrate: React.FC = () => {
   const [ratesData, setRatesData] = useState<RateData | null>(null);
+  const [discountData, setDiscountData] = useState<DiscountData | null>(null);
+  const [checkData, setCheckData] = useState<DiscountData | null>(null);
 
   useEffect(() => {
     fxrate();
+    discountRate();
   }, []);
 
   const data = {
@@ -20,6 +27,42 @@ const Fxrate: React.FC = () => {
       조회일자: "20230901",
       통화코드: "USD",
     },
+  };
+
+  const disdata = {
+    dataHeader: {
+      apikey: "2023_Shinhan_SSAFY_Hackathon",
+    },
+    dataBody: {
+      serviceCode: "T0501",
+    },
+  };
+
+  const discountRate = async () => {
+    try {
+      const response = await axios.post(
+        "/api1/v1/search/fx/discount-rate",
+        disdata
+      );
+      console.log(response.data.dataBody.리스트);
+      // setDiscountData(response.data.dataBody.리스트);
+
+      if (Array.isArray(response.data.dataBody.리스트)) {
+        const foundItem = response.data.dataBody.리스트.find(
+          (item: DiscountData) => item.통화.includes("USD")
+        );
+        if (foundItem) {
+          console.log("찾은 항목:", foundItem);
+          setCheckData(foundItem);
+        } else {
+          console.log("CAD를 포함한 항목을 찾지 못했습니다.");
+        }
+      } else {
+        console.log("dataBody.리스트가 배열이 아닙니다.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const fxrate = async () => {
@@ -46,6 +89,8 @@ const Fxrate: React.FC = () => {
       Fxrate
       <div>통화별환전조회</div>
       {renderRates()}
+      <div>USD우대율 조회</div>
+      {checkData?.우대율}
     </div>
   );
 };
