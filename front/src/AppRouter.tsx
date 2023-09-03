3a2d372b756021fb224d0c1fad18af36bd500955
최상익 <qjxltm@naver.com>
2023-09-03 20:30:18 +0900

    feat: 환전신청페이지 작성

--- front/src/AppRouter.tsx
+++ front/src/AppRouter.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import StudentIdPage from "./pages/StudentIdPage";
-import Fxrate from "./pages/Fxrate";
+import FxratePage from "./pages/FxratePage";
 import BankLocationPage from "./pages/BankLocationPage";
+import FxrequestPage from "./pages/FxrequestPage";
 
 function AppRouter() {
   return (
@@ -12,7 +13,8 @@ function AppRouter() {
         <Routes>
           <Route path="/Main" element={<MainPage />}></Route>
           <Route path="/StudentId" element={<StudentIdPage />}></Route>
-          <Route path="/Fxrate" element={<Fxrate />}></Route>
+          <Route path="/Fxrate" element={<FxratePage />}></Route>
+          <Route path="/Fxrequest" element={<FxrequestPage />}></Route>
           <Route path="/BankLocation" element={<BankLocationPage />}></Route>
         </Routes>
       </BrowserRouter>
---
+++ front/src/components/FxratePage/DiscountRate.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import api from "../../utils/api";
+
+interface DiscountData {
+  통화: string;
+  우대율: string;
+}
+
+interface DiscountRateProps {
+  selectedCurrency: string;
+}
+const DiscountRate: React.FC<DiscountRateProps> = ({ selectedCurrency }) => {
+  const [discountData, setDiscountData] = useState<DiscountData | null>(null);
+  const [checkData, setCheckData] = useState<DiscountData | null>(null);
+
+  const disdata = {
+    dataHeader: {
+      apikey: "2023_Shinhan_SSAFY_Hackathon",
+    },
+    dataBody: {
+      serviceCode: "T0501",
+    },
+  };
+
+  useEffect(() => {
+    discountRate(selectedCurrency);
+  }, [selectedCurrency]);
+
+  const discountRate = async (currency: string) => {
+    try {
+      const response = await api.post("/search/fx/discount-rate", disdata);
+      console.log(response.data.dataBody.리스트);
+
+      if (Array.isArray(response.data.dataBody.리스트)) {
+        const foundItem = response.data.dataBody.리스트.find(
+          (item: DiscountData) => item.통화.includes(currency)
+        );
+        if (foundItem) {
+          console.log("찾은 S항목:", foundItem);
+          setCheckData(foundItem);
+        } else {
+          console.log(`${currency}를 포함한 항목을 찾지 못했습니다.`);
+          setCheckData(null);
+        }
+      } else {
+        console.log("dataBody.리스트가 배열이 아닙니다.");
+        setCheckData(null);
+      }
+    } catch (error) {
+      console.log(error);
+      setCheckData(null);
+    }
+  };
+
+  return (
+    <div>
+      <p> DiscountRate</p>
+      {checkData
+        ? `${selectedCurrency} 우대율: ${checkData.우대율}`
+        : "우대율이 없습니다"}
+    </div>
+  );
+};
+
+export default DiscountRate;
