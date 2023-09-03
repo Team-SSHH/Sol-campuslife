8bddacb0a031a568f863a80ddf7a9fee940ac52c
윤자현 <bajh323@naver.com>
2023-09-03 15:54:53 +0900

    feat:Create FriendIDCard

--- front/src/components/StudentIdPage/StudentId.tsx
+++ front/src/components/StudentIdPage/StudentId.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./StudentId.css";
+import FrinedId from "./FrinedId";
 
 const StudentId = () => {
   return (
@@ -16,6 +17,20 @@ const StudentId = () => {
             <span>이상훈</span>
             <span>201403808</span>
           </div>
+        </div>
+      </div>
+      <div className="frinedsInfo">
+        <div className="friendsCategorySet">
+          {/* 나중에 포문 돌려서 넣기 */}
+          <div className="friendsCategory">전체보기</div>
+          <div className="friendsCategory">그룹 1</div>
+          <div className="friendsCategory">그룹 2</div>
+          <div className="friendsCategory">그룹 3</div>
+          {/* <div className="friendsCategory">그룹 4</div> */}
+        </div>
+        <div className="firendsList">
+          {/* 카데고리별 받아서 포문으로 보여주깅 */}
+          <FrinedId />
         </div>
       </div>
     </div>
