a6aab805c6bc2f31f2c1edf6b2cc6abceed0f4dd
최상익 <qjxltm@naver.com>
2023-09-03 20:57:33 +0900

    feat: 환전신청 및 결과 보기 api 연동

--- front/src/pages/FxrequestPage.tsx
+++ front/src/pages/FxrequestPage.tsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
 import api from "../utils/api";
 import { fxlist } from "../utils/fxlist";
+import Fxrequest from "../components/FxrequestPage/Fxrequest";
+import KrwAmountRequest from "../components/FxrequestPage/KrwAmountRequest";
+import Fxresult from "../components/FxrequestPage/Fxresult";
 
 const FxrequestPage: React.FC = () => {
   const [selectedCurrency, setSelectedCurrency] = useState<string>("USD");
-
+  const [inputAmount, setInputAmount] = useState<string>("");
   const handleCurrencyChange = (
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
     setSelectedCurrency(event.target.value);
   };
-
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputAmount(e.target.value);
+  };
   return (
     <div>
       <p>환전신청 페이지</p>
@@ -29,6 +34,19 @@ const FxrequestPage: React.FC = () => {
           ))}
         </select>
       </div>
+      <div>
+        <p>원화예상금액보기</p>
+        <input type="text" value={inputAmount} onChange={handleInputChange} />
+        <KrwAmountRequest
+          selectedCurrency={selectedCurrency}
+          inputAmount={inputAmount}
+        />
+      </div>
+      <Fxrequest
+        selectedCurrency={selectedCurrency}
+        inputAmount={inputAmount}
+      />
+      <Fxresult />
     </div>
   );
 };
