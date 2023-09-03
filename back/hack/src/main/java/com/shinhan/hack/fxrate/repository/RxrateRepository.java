3ae77dfae060f3c24aa3fdc7c67fe3c4c9fb4515
LeeSanghun <hoonideal@naver.com>
2023-09-03 21:21:07 +0900

    feat: 송금 초기 설정

---
+++ back/hack/src/main/java/com/shinhan/hack/fxrate/repository/RxrateRepository.java
@@ -0,0 +1,7 @@
+package com.shinhan.hack.fxrate.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RxrateRepository {
+}
---
+++ back/hack/src/main/java/com/shinhan/hack/fxrate/service/RxrateService.java
@@ -0,0 +1,7 @@
+package com.shinhan.hack.fxrate.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RxrateService {
+}
