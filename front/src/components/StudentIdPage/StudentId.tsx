343ff321f7b4d64e565d298c5ecfdfcb1ad76a69
윤자현 <bajh323@naver.com>
2023-09-05 01:01:30 +0900

    style: Update MainPage

--- front/src/components/StudentIdPage/StudentId.tsx
+++ front/src/components/StudentIdPage/StudentId.tsx
@@ -6,16 +6,16 @@ const StudentId = () => {
     <div>
       <div className="studentIdWrapper">
         <div className="studentInfo">
-          <div>이상훈 얼굴</div>
+          {/* <div>이상훈 얼굴</div> */}
           <img src={sanghoon} alt="sang" style={{ width: "100px" }} />
-          <div>
+          <div className="studentTextInfo">
             <span>금속신소재공학과</span>
             <span> </span>
             <span>재학생 (2학년)</span>
-          </div>
-          <div>
-            <span>이상훈</span>
-            <span>201403808</span>
+            <div>
+              <span>이상훈 </span>
+              <span>(201403808)</span>
+            </div>
           </div>
         </div>
       </div>
