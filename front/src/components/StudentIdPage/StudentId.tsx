d8b3f9a6465f6905782be589df9ecf90db1bee1c
donghyun <kdhdh001@naver.com>
2023-09-05 23:23:43 +0900

    feat: card뒷면

--- front/src/components/StudentIdPage/StudentId.tsx
+++ front/src/components/StudentIdPage/StudentId.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./StudentId.css";
 import sanghoon from "../../assets/sanghoon.png";
+
 const StudentId = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
-    <div>
-      <div className="studentIdWrapper">
+    <div
+      className={`studentIdWrapper ${isFlipped ? "flipped" : ""}`}
+      onClick={handleClick}
+    >
+      <div className="front">
         <div className="studentInfo">
           {/* <div>이상훈 얼굴</div> */}
           <img src={sanghoon} alt="sang" style={{ width: "100px" }} />
@@ -17,6 +27,13 @@ const StudentId = () => {
               <span>(201403808)</span>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="back">
+        <div className="cardInfo">
+          <div>계좌번호: 93087254624787</div>
+          <div>잔액: 500원</div>
         </div>
       </div>
     </div>
