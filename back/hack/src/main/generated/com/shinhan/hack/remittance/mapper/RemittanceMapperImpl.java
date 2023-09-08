bdc4d40e61680571bad75637837305582ced5427
LeeSanghun <hoonideal@naver.com>
2023-09-09 02:16:44 +0900

    fix : RemittanceMapperImpl 추가

---
+++ back/hack/src/main/generated/com/shinhan/hack/remittance/mapper/RemittanceMapperImpl.java
@@ -0,0 +1,11 @@
+package com.shinhan.hack.remittance.mapper;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-09-09T00:47:15+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.17 (Oracle Corporation)"
+)
+public class RemittanceMapperImpl implements RemittanceMapper {
+}
