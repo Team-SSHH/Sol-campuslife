4c5ef1263da1eb21d033ac7bdc1b9dcc8327155f
LeeSanghun <hoonideal@naver.com>
2023-09-13 01:01:28 +0900

    refactor: 친구 api 예외 처리

--- back/hack/src/main/java/com/shinhan/hack/friends/dto/FriendsDto.java
+++ back/hack/src/main/java/com/shinhan/hack/friends/dto/FriendsDto.java
@@ -2,24 +2,17 @@ package com.shinhan.hack.friends.dto;
 
 import com.shinhan.hack.category.entity.Category;
 import com.shinhan.hack.login.dto.StudentDto;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class FriendsDto {
     private Long fId;
     private Long categoryId;
     private String category;
     private Long studentId;
     private StudentDto.Response friend;
-
-    public FriendsDto(Long fId, Long categoryId, String category,
-                      Long studentId, StudentDto.Response friend) {
-        this.fId = fId;
-        this.categoryId = categoryId;
-        this.category = category;
-        this.studentId = studentId;
-        this.friend = friend;
-    }
 }
