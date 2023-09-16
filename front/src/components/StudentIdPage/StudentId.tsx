bea0b8eeca52392270c120a209f444cc63304490
윤자현 <bajh323@naver.com>
2023-09-17 03:10:02 +0900

    feat: 위도 경도 저장하기

--- front/src/components/StudentIdPage/StudentId.tsx
+++ front/src/components/StudentIdPage/StudentId.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./StudentId.css";
-import sanghoon from "../../assets/sanghoon.png";
 import SmartId from "../common/SmartId";
 import styled from "styled-components";
 import { useRecoilState } from "recoil";
 import { loginuser } from "../../stores/atoms";
 import { getMyMoney } from "../../services/apiService";
 import { formatCurrency } from "../common/formatCurrency";
+import usePostGPS from "../../hooks/usePostGPS";
+import usePostGPSState from "../../hooks/usePostGPSState";
+import useGPSLocation from "../../hooks/useGPSLocation";
 
 const StudentIdComponent = styled.div`
   width: 90%;
@@ -23,6 +25,24 @@ const StudentId = () => {
   const [isFlipped, setIsFlipped] = useState(false);
   const [userData, setUserData] = useRecoilState(loginuser);
   const [balance, setBalance] = useState(0);
+  const [isToggled, setIsToggled] = useState(userData.locationState);
+
+  const { handlePutMyLocationState } = usePostGPSState();
+  const { handlePutMyLocation } = usePostGPS();
+  const MyLocation = useGPSLocation();
+
+  useEffect(() => {
+    // true일때만 공유하는 것
+    // true 일 때, 내 위치 바뀔 때 마다 데이터 저장하기
+    if (isToggled === true) {
+      console.log("디비에 저장한다.", isToggled);
+      handlePutMyLocation(userData.studentId);
+    }
+  }, [MyLocation]);
+
+  useEffect(() => {
+    setIsToggled(userData.locationState);
+  }, [userData.locationState]);
 
   const handleClick = () => {
     setIsFlipped(!isFlipped);
@@ -41,13 +61,37 @@ const StudentId = () => {
     }
   };
 
+  const onToggle = () => {
+    const nextToggleState = !isToggled;
+
+    // 토글을 먼저 바꾼다.
+    setIsToggled(nextToggleState);
+
+    // 바꾼걸 저장한다.
+    handlePutMyLocationState(userData.studentId, nextToggleState);
+    setUserData((prevData: any) => ({
+      ...prevData,
+      locationState: nextToggleState,
+    }));
+
+    console.log("토글토글", isToggled);
+    console.log("내위치가 진짜 바로 바겻나요", userData.locationState);
+  };
+
   return (
     <StudentIdComponent>
-      <div
-        className={` ${isFlipped ? "flipped" : ""}`}
-        // onClick={handleClick}
-      >
+      <div className={` ${isFlipped ? "flipped" : ""}`}>
         <div className="front">
+          <div
+            className={`toggle-button ${isToggled ? "toggled" : ""}`}
+            onClick={(e) => {
+              onToggle();
+              e.stopPropagation();
+            }}
+          >
+            <div className="toggle-circle"></div>
+          </div>
+
           <SmartId
             name={userData.name}
             major={userData.major}
