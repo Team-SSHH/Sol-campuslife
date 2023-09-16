44b4d6eefd5302a2e4ee23fb8f2b532b521320fe
김동현 <kdhdh001@naver.com>
2023-09-16 16:08:52 +0900

    feat: 현재 위치에서 가까운 영업점완료

--- front/src/hooks/useShinhanLocation.tsx
+++ front/src/hooks/useShinhanLocation.tsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import { postShinhanLocation } from "../services/apiService";
 import { BankLocationType } from "../types/DataType";
+import useGPSLocation from "./useGPSLocation";
+
 const useShinhanLocation = (cityName: String) => {
   const [bankLocationData, setbankLocationData] = useState([]);
   const [BanknearbyKonKuk, setBanknearbyKonKuk] = useState<BankLocationType[]>(
     []
   );
+  const [BanknearbyMyLocation, setBanknearbyMyLocation] = useState<
+    BankLocationType[]
+  >([]);
+
+  const MyLocation = useGPSLocation();
 
   const calculateDistance = (lat1: any, lon1: any, lat2: any, lon2: any) => {
     var R = 6371e3; // metres
@@ -28,6 +35,7 @@ const useShinhanLocation = (cityName: String) => {
         const response = await postShinhanLocation(cityName);
         setbankLocationData(response.data.dataBody.리스트);
 
+        // 건국대 주변은행!!!
         // 반경 내에 있는 은행 지점만 필터링
         let nearbyKonKuk = response.data.dataBody.리스트.filter(
           (location: any): boolean =>
@@ -38,6 +46,7 @@ const useShinhanLocation = (cityName: String) => {
               parseFloat("127.078959")
             ) <= 1000
         );
+
         // 거리에 따라 정렬
         nearbyKonKuk = nearbyKonKuk.sort(
           (a: any, b: any) =>
@@ -54,18 +63,49 @@ const useShinhanLocation = (cityName: String) => {
               parseFloat("127.078959")
             )
         );
+        // 내위치 주변은행!!!
+        // 내 위치 주변은행 찾기
+        let nearbyMylocation: BankLocationType[] = [];
+
+        if (MyLocation.latitude !== null && MyLocation.longitude !== null) {
+          nearbyMylocation = response.data.dataBody.리스트.filter(
+            (location: any): boolean =>
+              calculateDistance(
+                parseFloat(location.지점위도),
+                parseFloat(location.지점경도),
+                MyLocation.latitude,
+                MyLocation.longitude
+              ) <= 3500
+          );
+        }
+        // 거리에 따라 정렬
+        nearbyMylocation = nearbyMylocation.sort(
+          (a: any, b: any) =>
+            calculateDistance(
+              parseFloat(a.지점위도),
+              parseFloat(a.지점경도),
+              MyLocation.latitude,
+              MyLocation.longitude
+            ) -
+            calculateDistance(
+              parseFloat(b.지점위도),
+              parseFloat(b.지점경도),
+              MyLocation.latitude,
+              MyLocation.longitude
+            )
+        );
 
         setBanknearbyKonKuk(nearbyKonKuk);
+        setBanknearbyMyLocation(nearbyMylocation);
       } catch (error) {
         console.log(error);
       }
     };
 
     fetchShinhanLocation();
-  }, [cityName]);
-  // console.log(nearbyKonKuk);
-  // console.log("dddddddddddnearbyKonKuk");
-  return { bankLocationData, BanknearbyKonKuk };
+  }, [cityName, MyLocation]);
+
+  return { bankLocationData, BanknearbyKonKuk, BanknearbyMyLocation };
 };
 
 export default useShinhanLocation;
